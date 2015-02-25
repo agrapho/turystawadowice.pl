582e31ba2c9e184d36002f2740890199a71fd598
Kuba Odias <agrapho.pl@gmail.com>
2015-02-25 22:11:48 +0100

    Link to offer.

--- sidebar.php
+++ sidebar.php
@@ -10,10 +10,12 @@
 	        print $ep3gate->getPart('footer');
 	    ?>
 
-	    <div id="sidebar-offer" class="with-shadow">
-		<img class="bg" src="<?php echo get_stylesheet_directory_uri()."/images/offer-bg.png"; ?>" alt="" />
-		<img class="fg" src="<?php echo get_stylesheet_directory_uri()."/images/offer-fg.png"; ?>" alt="" />
-            </div>
+	    <a href="/kontakt">
+                <div id="sidebar-offer" class="with-shadow">
+		    <img class="bg" src="<?php echo get_stylesheet_directory_uri()."/images/offer-bg.png"; ?>" alt="" />
+		    <img class="fg" src="<?php echo get_stylesheet_directory_uri()."/images/offer-fg.png"; ?>" alt="" />
+                </div>
+            </a>
 
 	    <div id="partnership-programme">
 		<object width="195" height="195" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0">
